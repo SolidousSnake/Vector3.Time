ee86c9160599b881908a9564894573d4a24dda8e
Timour <67632745+SolidousSnake@users.noreply.github.com>
2024-12-27 23:35:05 +0300

    Initial commit

---
+++ VectorToTimeExtension.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace _Project.Code.Utils
+{
+    public static class VectorToTimeExtension
+    {
+        /// <summary>
+        /// Calculates the time required to travel the distance represented by the Vector3 with a given speed.
+        /// </summary>
+        /// <param name="distance">The distance as a Vector3.</param>
+        /// <param name="speed">The speed of travel (must be greater than zero).</param>
+        /// <returns>The time required to travel the distance. Returns float.PositiveInfinity if speed is zero.</returns>
+        public static float Time(this Vector3 distance, float speed)
+        {
+            if (speed <= 0f)
+            {
+                Debug.LogWarning("Bro, how did you get a negative speed?");
+                return float.PositiveInfinity;
+            }
+
+            float distanceMagnitude = distance.magnitude;
+            return distanceMagnitude / speed;
+        }
+    }
+}
